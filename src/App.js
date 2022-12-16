255772766fcb33f53894855a808a1e4cc9ed6063
kanwar Dhillon <kdhillon469@gmail.com>
2022-12-17 09:54:10 +1100

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 
-import './App.css';
+import { useState } from 'react';
+
+import useMediaQuery from './hooks/useMediaQuery';
+import Navbar from './scenes/Navbar';
 
 function App() {
+  const [selectedPage,setSelectedPage]=useState("home")
+  const isAboveMediaScreen=useMediaQuery("(min-width:1060px)")
   return (
-    <div className="App ">
+    <div className="app bg-deep-blue ">
+      <Navbar selectedPage={selectedPage}
+       setSelectedPage={setSelectedPage} 
+      />
     </div>
   );
 }
