8869ff87106db79ec10bde81820347ce5629d61f
kanwar Dhillon <kdhillon469@gmail.com>
2022-12-20 11:30:49 +1100

    skills section added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
+import { useEffect, useState } from "react";
 
-import { useState } from 'react';
-
-import useMediaQuery from './hooks/useMediaQuery';
-import Navbar from './scenes/Navbar';
+import useMediaQuery from "./hooks/useMediaQuery";
+import DotGroup from "./scenes/DotGroup";
+import Navbar from "./scenes/Navbar";
+import Landing from "./scenes/Landing"
+import MySkills from "./scenes/Skills";
+import LineGradient from "./components/LineGradient";
+import { motion } from "framer-motion";
 
 function App() {
-  const [selectedPage,setSelectedPage]=useState("home")
-  const isAboveMediaScreen=useMediaQuery("(min-width:1060px)")
+  const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setisTopOfPage] = useState(true);
+
+  const isAboveMediaScreen = useMediaQuery("(min-width:1060px)");
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY === 0 ? setisTopOfPage(true) : setisTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+
+  }, []);
+
   return (
     <div className="app bg-deep-blue ">
-      <Navbar selectedPage={selectedPage}
-       setSelectedPage={setSelectedPage} 
+      <Navbar
+      isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+        isAboveMediaScreen={isAboveMediaScreen}
       />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediaScreen && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+        </div>
+        <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full ">
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("skills")}
+        >
+          <MySkills />
+        </motion.div>
+      </div>
     </div>
   );
 }
