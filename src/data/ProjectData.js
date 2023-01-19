e687d35f351117fe8ea101ad79cd887d12559ded
kanwar Dhillon <kdhillon469@gmail.com>
2023-01-20 10:13:04 +1100

    assets added

---
+++ src/data/ProjectData.js
@@ -0,0 +1,34 @@
+export  const ProjectList= [
+    {
+     title:'Shopping Cart',
+     imgUrl:'project-shoppingcart.png',
+     desc:'A react shopping cart app using react,hooks,context Api and useReducer hook.',
+     stack:'React,Hooks,Context APi,Reducers',
+     demo:'https://shopping-cart-kdhillon29.vercel.app/',
+     github:'https://github.com/kdhillon29/shopping-cart'
+    },
+    {
+        title:'RealEstate',
+    imgUrl:'project-realestate.png',
+    desc:'A realestate web app using Angular and firebase.User can register and login to add new property.',
+    stack:'Angular,RxJS,Firebase Auth,Firestore Database',
+    demo:'https://desiestate-63d59.web.app/',
+    github:'https://github.com/kdhillon29/DesiEstate'
+   },
+   {
+    title:'Portfolio',
+    imgUrl:'project-portfolio.png',
+    desc:'A portfolio web App to showcase skills.Responsive layout using Tailwindcss and animation using framer motion .',
+    stack:'React,Framer motion,Hooks,React-hooks-form',
+    demo:'https://react-portfolio-kdhillon.vercel.app/',
+    github:'https://github.com/kdhillon29/react-portfolio'
+   },
+   {
+   title:'Travel Blog',
+   imgUrl:'project-trvl.png',
+   desc:'A travel Blog app developed using react and styled-components.',
+   stack:'ReactJS,Styled-components,CSS3',
+   demo:'https://trvel.vercel.app/',
+   github:'https://github.com/kdhillon29/trvel'
+  }
+]
